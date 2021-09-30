33a39900ddc83e565635f42b4ef7b30465cb38ab
Nguyen Hoai Khang <nguyenhoaikhang37@gmail.com>
2021-09-30 21:44:24 +0700

    test

--- src/Views/UserManager/EditUser/index.jsx
+++ src/Views/UserManager/EditUser/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../../../HOC/Layout";
 
 const EditUser = () => {
-  return <Layout>sửa thông tin</Layout>;
+  return <Layout>sửa thông tain</Layout>;
 };
 
 export default EditUser;
